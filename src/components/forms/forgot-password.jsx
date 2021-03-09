da55c810286130590b14d3b915ba803095dc33e8
Tom Schrier <tomschr@stud.ntnu.no>
2021-03-09 17:51:25 +0100

    imporve CSS

--- src/components/forms/forgot-password.jsx
+++ src/components/forms/forgot-password.jsx
@@ -5,15 +5,18 @@ import './forgot-password.css';
 class ForgotPassword extends Component {
     render() {
         return (
-            <div className="ForgotPasswordForm">
-                <form>
-                    <fieldset>
-                    <legend>Forgot Password</legend>
-                        <EmailInput />
-                        <button>Reset Password</button>
-                    </fieldset>
-                </form>
-            </div>
+            <>
+                <p>You will receive an email that will help you create a new password for your account.</p>
+                <div className="ForgotPasswordForm">
+                    <form>
+                        <fieldset>
+                            <legend>Forgot Password</legend>
+                            <EmailInput />
+                            <button>Reset Password</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </>
         );
     }
 }
